fc601aa928cba3c853e2051d2f69352878f98fac
gtejasri <tejusmiley999@gmail.com>
2021-10-28 17:49:51 +0530

    commit

--- src/Lab2/q1/SmallestArray.java
+++ src/Lab2/q1/SmallestArray.java
@@ -1,7 +1,7 @@
 package Lab2.q1;
 
 import java.util.Arrays;
-
+import java.util.Scanner;
 public class SmallestArray {
 
 	public static int getSecondSmallest(int[] a, int total) {
@@ -12,8 +12,17 @@ public class SmallestArray {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int a[] = { 88, 44, 66, 99, 33, 77, 22, 11, 55 };
-		System.out.println("Second Smallest: " + getSecondSmallest(a, 8));
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter size");
+		int n=sc.nextInt();
+		
+		int arr[]=new int[n];
+		System.out.println("Enter Elements");
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		//int a[] = { 88, 44, 66, 99, 33, 77, 22, 11, 55 };
+		System.out.println("Second Smallest: " + getSecondSmallest(arr, n));
 
 	}
 
